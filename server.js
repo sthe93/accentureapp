5f767d0dae3e4ad49efcf161559d8a23487c6f54
Sithembiso Chivambo <schivambo@jhb.dvt.co.za>
2023-09-19 09:48:20 +0200

    Add server.js e

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Start the server
+// Start the 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
